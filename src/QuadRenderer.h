50df2632e7d5fc72bf31be4cdbf4a53b02c4ce6c
artn-dev <azemarteixeira@gmail.com>
2021-03-17 19:05:23 -0300

    Added implementation file for renderer

--- src/QuadRenderer.h
+++ src/QuadRenderer.h
@@ -18,81 +18,15 @@ private:
         static constexpr unsigned int MAX_QUAD_AMOUNT = 1000;
 
 public:
-        QuadRenderer(ShaderProgram *shader)
-                : shader_(shader), queued(0), render_data(nullptr)
-        {
-                glGenBuffers(1, &vbo);
-                glGenVertexArrays(1, &vao);
-                init_buffer_layout();
+        QuadRenderer(ShaderProgram *shader);
+        ~QuadRenderer();
 
-                render_data = new Quad[MAX_QUAD_AMOUNT];
-        }
-
-        ~QuadRenderer()
-        {
-                glDeleteBuffers(1, &vbo);
-                glDeleteVertexArrays(1, &vao);
-
-                delete [] render_data;
-        }
-
-        void render()
-        {
-                glUseProgram(shader_->id());
-                glBindVertexArray(vao);
-
-                glBindBuffer(GL_ARRAY_BUFFER, vbo);
-                glBufferSubData(GL_ARRAY_BUFFER, 0, queued * sizeof(Quad), render_data);
-
-                glDrawArrays(GL_POINTS, 0, queued);
-                
-                queued = 0;
-        }
-
-        void batch(Quad quad)
-        {
-                render_data[queued] = quad;
-                queued++;
-        }
-
-        void batch(Quad *quads, unsigned int count)
-        {
-                for (unsigned int i = 0; i < count; i++)
-                        batch(quads[i]);
-        }
+        void render();
+        void batch(Quad quad);
+        void batch(Quad *quads, unsigned int count);
 
 private:
-        void init_buffer_layout()
-        {
-                glBindVertexArray(vao);
-                glBindBuffer(GL_ARRAY_BUFFER, vbo);
-
-                glBufferData(
-                        GL_ARRAY_BUFFER, MAX_QUAD_AMOUNT * sizeof(Quad),
-                        nullptr, GL_DYNAMIC_DRAW
-                );
-
-                glEnableVertexAttribArray(0);
-                glVertexAttribPointer(
-                        0, 2, GL_FLOAT, GL_FALSE, sizeof(Quad),
-                        (void*)offsetof(Quad, position)
-                );
-
-                glEnableVertexAttribArray(1);
-                glVertexAttribPointer(
-                        1, 4, GL_FLOAT, GL_FALSE, sizeof(Quad),
-                        (void*)offsetof(Quad, color)
-                );
-
-                glEnableVertexAttribArray(2);
-                glVertexAttribPointer(
-                        2, 1, GL_FLOAT, GL_FALSE, sizeof(Quad),
-                        (void*)offsetof(Quad, padding)
-                );
-
-                glBindBuffer(GL_ARRAY_BUFFER, 0);
-                glBindVertexArray(0);
-        }
+        void init_buffer_layout();
 
 };
 
